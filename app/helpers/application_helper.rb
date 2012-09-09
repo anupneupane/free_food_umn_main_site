3ce6b78032a2f26307cb1c7ce4a2c84b2ffc6ed8
Zoltan Kiss <kissx012@umn.edu>
2012-09-09 13:40:10 -0500

    fixed bugs with month view
    there was a bug with going from December X to January X+1, and vica-versa.
    Also the numeric days were not aligned with the Mon..Fri day representations.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,6 +18,7 @@ module ApplicationHelper
       location_exists: location_exists, google_maps_url_exists: google_maps_url_exists,
       location: location, google_maps_url: google_maps_url
   end
+
   def format_hours date
     return date.strftime("%l:%M %P")
   end
@@ -25,4 +26,29 @@ module ApplicationHelper
   def flexible_line_height_link_to text, href
     render partial: 'shared/paragraph_hyper_link', locals: { href: href, text: text }
   end
+
+  def next_month month
+    month_adjusted_for_modulous = month - 1
+    month_adjusted_for_modulous += 1
+    month_adjusted_for_modulous_mod = month_adjusted_for_modulous % 12
+    return month_adjusted_for_modulous_mod + 1
+  end
+
+  def previous_month month
+    month_adjusted_for_modulous = month - 1
+    month_adjusted_for_modulous -= 1
+    month_adjusted_for_modulous = 11 if month_adjusted_for_modulous < 0
+    month_adjusted_for_modulous_mod = month_adjusted_for_modulous % 12
+    return month_adjusted_for_modulous_mod + 1
+  end
+
+  def next_months_year month, year
+    month == 12 ? year + 1 : year
+  end
+
+
+  def previous_months_year month, year
+    month == 1 ? year - 1 : year
+  end
+
 end
