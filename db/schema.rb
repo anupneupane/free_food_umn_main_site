96f08aaf6a1cd5fb3663ad647ee2ff2e4fb008e0
Zoltan Kiss <kissx012@umn.edu>
2012-09-09 14:01:57 -0500

    updated db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120908021108) do
+ActiveRecord::Schema.define(:version => 20120909180223) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -46,12 +46,23 @@ ActiveRecord::Schema.define(:version => 20120908021108) do
     t.text     "long_description"
   end
 
-  create_table "groups", :force => true do |t|
+  create_table "organizations", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.string   "name"
-    t.text     "description"
-    t.text     "current_main_event"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
   end
+
+  add_index "organizations", ["email"], :name => "index_organizations_on_email", :unique => true
+  add_index "organizations", ["reset_password_token"], :name => "index_organizations_on_reset_password_token", :unique => true
 
 end
