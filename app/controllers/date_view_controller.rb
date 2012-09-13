e777bf8df3ec284b52db0647cb976dabd7799957
Zoltan Kiss <kissx012@umn.edu>
2012-09-13 14:59:50 -0500

    refactor DateViewController, and remove no longer necessary PagesController

--- app/controllers/date_view_controller.rb
+++ app/controllers/date_view_controller.rb
@@ -1,20 +1,27 @@
 
 class DateViewController < DateViewAndUiController
 
+  before_filter :set_events_var
+
   def view_by_month
-    @events = Event.where(:approved_by_admin => true)
     @calendar_year = (params[:year] || DateTime.now.year).to_i
     @calendar_month = (params[:month] || DateTime.now.month).to_i
     @date_group = DateTime.new(@calendar_year, @calendar_month, 1).strftime("%B, %Y")
   end
 
   def view_by_week
-    @events = Event.where(:approved_by_admin => true)
     year = (params[:year] || DateTime.now.year).to_i
     month = (params[:month] || DateTime.now.month).to_i
     day = (params[:day] || DateTime.now.day).to_i
     @starting_day = DateTime.new(year, month, day)
     @date_group = @starting_day.strftime("the week of %_m/%d/%Y")
   end
+
+  private
+    def set_events_var
+      @events_of_approved_organizations = Event.all(:joins => :organization, :conditions => { :organizations => { :approved_by_admin => true } })
+      @approved_events = Event.where(:approved_by_admin => true).all
+      @events = @events_of_approved_organizations + @approved_events
+    end
 
 end
