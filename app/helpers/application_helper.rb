6d26079b5fe49326e9d35631c913fb95bb8f41ea
Zoltan Kiss <kissx012@umn.edu>
2012-09-13 14:58:38 -0500

    remove unnecessary code from application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,22 +3,6 @@ module ApplicationHelper
     render 'shared/menu', :menu_items => menu_items
   end
 
-
-
-#<% if @event.location.nil? and not @event.google_maps_url.nil? %>
-#  <p><%= link_to("google maps", raw(@event.google_maps_url)) %></p>
-#<% elsif not @event.location.nil? and not @event.google_maps_url.nil? %>
-#  <p><%= link_to(@event.location, raw(@event.google_maps_url)) %></p>
-#<% elsif not @event.location.nil? and @event.google_maps_url.nil? %>
-#  <p>location: <%= @event.location %></p>
-#<% end %>
-  def best_html_if_maybe_location_and_maybe_google_maps_url location_exists, google_maps_url_exists,
-                                                            location, google_maps_url, text_before_location
-    render 'shared/best_html_if_maybe_location_and_maybe_google_maps_url',
-      location_exists: location_exists, google_maps_url_exists: google_maps_url_exists,
-      location: location, google_maps_url: google_maps_url, text_before_location: text_before_location
-  end
-
   def format_hours date
     return date.strftime("%l:%M %P")
   end
