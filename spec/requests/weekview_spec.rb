5905ae7c1c87c22cc9ccdb1772bb43e74fabcab8
Zoltan Kiss <kissx012@umn.edu>
2012-10-23 17:06:21 -0500

    week view spec

---
+++ spec/requests/weekview_spec.rb
@@ -0,0 +1,54 @@
+require "spec_helper"
+
+describe "Weekview" do
+
+  describe "current date" do
+    describe "not admin-approved" do
+      before do
+        Event.create!(name: "not admin-approved",
+                      description: "New event",
+                      date_string: "#{DateTime.now.year}-#{DateTime.now.month}-#{DateTime.now.day}",
+                      start_time_string: "10:10",
+                      meridian_indicator: "pm",
+                      duration: "2:20",
+                      google_maps_url: 'http://maps.google.com',
+                      location: "coffman")
+        visit "/view_by_week"
+      end
+    
+      it "Event created" do
+        Event.first.date.month.should == DateTime.now.month
+      end
+    
+      it "event shows up" do
+        page.should_not have_selector("a", text: "not admin-approved")
+      end
+    end
+
+    describe "admin-approved" do
+      before do
+        Event.create!(name: "admin-approved",
+                      description: "New event",
+                      date_string: "#{DateTime.now.year}-#{DateTime.now.month}-#{DateTime.now.day}",
+                      start_time_string: "10:10",
+                      meridian_indicator: "pm",
+                      duration: "2:20",
+                      google_maps_url: 'http://maps.google.com',
+                      location: "coffman")
+        event = Event.where(name: "admin-approved").first
+        event.update_column(:approved_by_admin, true)
+        visit "/view_by_week"
+      end
+    
+      it "Event created" do
+        Event.first.date.month.should == DateTime.now.month
+      end
+    
+      it "event shows up" do
+        page.should have_selector("a", text: "admin-approved")
+      end
+
+    end
+  end
+
+end
