143c861fa8afc601bc7bf11736e7ce093c403554
Zoltan Kiss <kissx012@umn.edu>
2012-11-09 21:00:13 -0600

    list_view/mobile view quick fix

--- app/controllers/date_view_controller.rb
+++ app/controllers/date_view_controller.rb
@@ -19,12 +19,22 @@ class DateViewController < DateViewAndUiController
 
   def mobile
     number_of_events_per_page = 30
-    @show_left_arrow = ((closest_to_today - number_of_events_per_page*(params[:n]).to_i) < 1) ? false : true
-    @show_right_arrow = ((closest_to_today + number_of_events_per_page*(params[:n]).to_i) > @events.length - 2) ? false : true
 
-    start_event_index = [closest_to_today - number_of_events_per_page*(params[:n]).to_i, 0].max
-    end_event_index = [start_event_index + number_of_events_per_page, @events.length-1].min
-    @events = @events[start_event_index, end_event_index]
+    closest_to_today = get_event_closed_to_today(@events)
+
+    @n = params[:n] || 0
+    offset = @n ? number_of_events_per_page * @n.to_i : 0
+    right_offset = @n ? number_of_events_per_page * (@n.to_i + 1) : 0
+
+    if closest_to_today.nil?
+      @show_left_arrow = false
+      @show_right_arrow = false
+    else
+      @show_left_arrow = (closest_to_today + offset < 1) ? false : true
+      @show_right_arrow = ((closest_to_today + right_offset) > @events.length - 2) ? false : true
+    end
+
+    @events = get_events_that_havent_happened_yet @events, number_of_events_per_page, offset
     render 'events/mobile', :layout => false
     return false
   end
@@ -38,13 +48,18 @@ class DateViewController < DateViewAndUiController
     offset = @n ? number_of_events_per_page * @n.to_i : 0
     right_offset = @n ? number_of_events_per_page * (@n.to_i + 1) : 0
 
-    @show_left_arrow = (closest_to_today + offset < 1) ? false : true
-    @show_right_arrow = ((closest_to_today + right_offset) > @events.length - 2) ? false : true
+    if closest_to_today.nil?
+      @show_left_arrow = false
+      @show_right_arrow = false
+    else
+      @show_left_arrow = (closest_to_today + offset < 1) ? false : true
+      @show_right_arrow = ((closest_to_today + right_offset) > @events.length - 2) ? false : true
+    end
 
     @events = get_events_that_havent_happened_yet @events, number_of_events_per_page, offset
 
     @datestring = "%B, %_d"
-    @date = @events.length > 0 ? DateTime.now : @events[0].date
+    @date = @events.length > 0 ? @events[0].date : DateTime.now
     @date = @date.strftime(@datestring)
   end
 
@@ -81,6 +96,7 @@ class DateViewController < DateViewAndUiController
       events_that_havent_happened_yet = events
       events_that_havent_happened_yet.sort_by! {|event| event.date}
       closest_to_today = get_event_closed_to_today(events_that_havent_happened_yet)
+      return [] if closest_to_today.nil?
 
       start_event_index = [closest_to_today + offset, 0].max
       return events_that_havent_happened_yet[start_event_index, number_of_events_per_page]
